7f5cfae80a51483b739c2b66aabb7af46ed82ca9
bandicoutts <d.coutts@gmail.com>
2019-02-21 20:14:02 +1300

    Update multiples.java
    Added exception handling for when a non-integer based value is entered, and for simplicity's sake just said that a random integer between 1-1000 will be generated and used instead.

--- multiples.java
+++ multiples.java
@@ -4,6 +4,8 @@ we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. */
 
 import java.util.Scanner;
+import java.util.*;
+import java.util.Random;
 
 
 class multiples {
@@ -11,11 +13,15 @@ class multiples {
 
     private static int getRange(){
         String result;
-        Scanner reader = new Scanner(System.in);
+        Scanner r = new Scanner(System.in);
         System.out.println("Enter a number to sum values up to:");
-        result = reader.nextLine();
-        int returnValue = Integer.parseInt(result);
-        return returnValue;
+        try {
+            return r.nextInt();
+        } catch (InputMismatchException e){
+            int random = (int)(Math.random() * 1000);
+            System.out.println("That's not a valid number. We'll just do this with the number " + random);
+            return random;
+        }
     }
 
     private static void doSum(int range){
